68f47a377c47ae8b75b9e4d9e4c4825345fddebe
manzaero <ligoero@gmail.com>
2023-09-17 21:06:26 +0300

    app.js cleaned

--- src/App.js
+++ src/App.js
@@ -14,15 +14,7 @@ const arr = [{
 }]
 
 function App() {
-    const [count, setCount] = useState(0)
-    const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1)
     return (<div className="wrapper">
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
-        </div>
         <Drawer/>
         <Header/>
         <div className="content p-10">
